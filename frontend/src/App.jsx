574b138939f23f212d117b347df8906316d9e641
Sumbul Rani <ranisumbul7@gmail.com>
2026-08-10 10:44:25 +0530

    Your message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 
 import "./App.css";
-
+import Navbar from "./components/shared/Navbar";
 function App() {
   return(
     <>  
-      <h1 className="text-red-500">Lets build job portal</h1>
+      <Navbar />
     </>
   )
 }
--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
-
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+import { Button } from '../ui/button'
+import { Avatar, AvatarImage } from "../ui/avatar"
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='bg-white'>
+        <div className="flex items-center justify-between h-16 mx-auto max-w-7xl">
+       <div>
+            <h1 className='text-2xl font-bold'>Job<spam className='text-[#F83002]'>Portal</spam></h1>
+            </div>
+            <div className='flex items-center gap-12'>
+                <ul className='flex items-center gap-5 font-medium'>
+                <li>Home</li>
+                <li>Jobs</li>
+                <li>Browse</li>
+            </ul>
+      <Popover>
+         <PopoverTrigger asChild>
+          <Avatar className="cursor-pointer">
+             <AvatarImage src="https://github.com/shadcn.png" />
+     </Avatar>        
+</PopoverTrigger>
+        <PopoverContent>
+             <h1>Helo</h1>
+        </PopoverContent>
+      </Popover>
+
+            </div>
+        </div>
+        </div>
+
+        
     )
 }
 
 export default Navbar;
+
