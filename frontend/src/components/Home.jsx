00e4abd9861586a77e90a94883633745fc67feed
Sumbul Rani <ranisumbul7@gmail.com>
2026-08-29 10:31:19 +0530

    Your message

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
 import CategoryCarousel from './CategoryCarousel'
+import LatestJobs from './LatestJobs'
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Navbar />
             <HeroSection />
             <CategoryCarousel />
+            <LatestJobs />
         </div>
     )
 }
---
+++ frontend/src/components/LatestJobs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LatestJobs = () => {
+   
+    return (
+        <div className='mx-auto my-20 max-w-7xl'>
+            <h1 className='text-4xl font-bold'><span className='text-[#6A38C2]'>Latest & Top </span> Job Openings</h1>
+            <div className='grid grid-cols-3 gap-4 my-5'>
+              
+            </div>
+        </div>
+    )
+}
+
+
+export default LatestJobs
